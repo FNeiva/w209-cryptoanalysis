ac4836c4c07887f275ab940860150de14886b67c
Arnobio Morelix <arnobiomorelix@Arnobios-MacBook-Pro.local>
2018-12-12 18:56:44 -0800

    add viz to slides, Store of Value

--- crypto.py
+++ crypto.py
@@ -189,6 +189,73 @@ cryptocurrency is relative to the historical performance of that currency in bot
 )
 
 ####################################################
+### 		STORE OF VALUE CODE				     ###
+####################################################
+
+df = pd.read_csv(
+    'crypto10-markets-am.csv')
+
+storevalue = html.Div(children = [
+    html.H1(children='How volatile are crypto prices?'),
+
+    
+    html.Div(children='''
+        % Change in Prices by Day for Top 10 Cryptocurrencies by Market Cap (2013-2018)
+        
+        Hover over the data points to see more details.
+        You can click on the currency names on the legends to add/eliminate them from the graph.
+    '''),
+
+    # graph 1
+    dcc.Graph(
+        id='change',
+        config = {
+            'displaylogo': False
+        },
+        figure={'data': [go.Scatter(
+            x = df[df['name'] == i].date,
+            y = df[df['name'] == i]['change%'],
+            name=i
+        )
+        for i in df.name.unique()],
+
+                
+                'layout': go.Layout(
+                xaxis={'type': 'date', 'title': 'Date'},
+                yaxis={'type': 'linear', 'title': '% Change in Day', 'tickformat': ',.000001%'}, 
+                margin={'l': 100, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest')
+
+        }
+        ),
+    
+    # graph 2
+    #dcc.Graph(
+        #id='summary',
+        #config = {
+            #'displaylogo': False},
+        #figure={'data': 
+                #[dict(type = 'scatter',
+                      #x = df['name'],
+                      #y = df['change%'],
+                      #mode = 'markers',
+                      #transforms = [dict(
+                      #    type = 'aggregate',
+                      #    groups = df['name'].unique(),
+                      #    aggregations = [dict(target = 'y', func = 'count', enabled = True),]
+                      #)]
+                     #)]
+
+               #}
+        #),
+])
+
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
+
+
+####################################################
 ### 		SETUP GENERAL HTML					 ###
 ####################################################
 
@@ -285,6 +352,10 @@ slide2 = html.Section(html.Div([navbar,
 ## SLIDE 3: Fast and cheap
 slide3 = html.Section(fastcheapviz)
 
+## SLIDE 4: Store of Value
+slide4 = html.Section(storevalue)
+
+
 ## TEST SLIDE
 slide_test = html.Section(html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -310,6 +381,8 @@ slide_test = html.Section(html.Div(children=[
 slides.append(slide1)
 slides.append(slide2)
 slides.append(slide3)
+slides.append(slide4)
+
 
 ####################################################
 ### 				DASH LAYOUT					 ###
